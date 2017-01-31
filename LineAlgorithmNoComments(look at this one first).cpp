3fa7d90618514eab385ad5fb8b71a10d22ad3428
IanAdok <ian.adok@gmail.com>
2017-01-31 21:00:07 +0100

    Update LineAlgorithmNoComments(look at this one first).cpp

--- LineAlgorithmNoComments(look at this one first).cpp
+++ LineAlgorithmNoComments(look at this one first).cpp
@@ -21,7 +21,7 @@ The task was:
 			and your function algorithm should take in a "vector of points"
 			
 			One of the things that is important in such a task is that you get a nested loop situation
-			that makes like 1 million turns in the deepest level.
+			that makes like a million turns in the deepest level.
 			This means the things you want to focus in the whole code is to optimize inside that and understand that
 			things like a slower hash, memory etc. function can cost alot when it's done alot of times.			
 */
