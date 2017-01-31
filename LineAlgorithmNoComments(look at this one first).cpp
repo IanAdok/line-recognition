93112832b275406fbd9d2b332b40a4fe303c6c37
IanAdok <ian.adok@gmail.com>
2017-01-31 21:06:59 +0100

    Update LineAlgorithmNoComments(look at this one first).cpp

--- LineAlgorithmNoComments(look at this one first).cpp
+++ LineAlgorithmNoComments(look at this one first).cpp
@@ -23,7 +23,8 @@ The task was:
 			One of the things that is important in such a task is that you get a nested loop situation
 			that makes like a million turns in the deepest level.
 			This means the things you want to focus in the whole code is to optimize inside that and understand that
-			the effectiveness of your hash and memory structure and techniques has a big impact if they are used inside.
+			the effectiveness of your hash and memory structure and techniques has a big impact.
+			(Never underestimate the rate things can scale!)
 */
 
 using namespace std;
